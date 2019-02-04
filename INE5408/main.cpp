6bbf14d894ce68a9441e3d5fcde124013817ed15
zDudaHang <sorveteblueice@live.com>
2019-02-04 21:13:39 -0200

    Deletando arquivos desnecessários

--- INE5408/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdint>
-#include <stdexcept>  // C++ exceptions
-#include <cstring>
-#include <stdio.h>
-int main(int argc, char** argv) {
-    char* entrada1 = nullptr, entrada2;
-    scanf("<%s> </%s>", entrada1, entrada2);
-    return 0;
-}
